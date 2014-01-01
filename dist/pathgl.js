3f1e36b4fd842e8d8ab2db8c02cc7696fa83529e
adnan wahab <mail@adnanwahab.com>
2014-01-01 01:30:37 -0600

    remove dead code

--- dist/pathgl.js
+++ dist/pathgl.js
@@ -551,64 +551,6 @@ function beforeRender(elapsed) {
   gl.clear(gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT)
 }
 
-function addToBuffer(datum) {
-  return extend(datum.path = [], { coords: [], id: datum.id })
-}
-
-function applyTransforms(node) {
-  gl.uniform2f(program.translate, node.attr.translate[0] + node.attr.cx + node.attr.x,
-               node.attr.translate[1] + node.attr.cy + node.attr.y)
-  gl.uniform2f(program.scale, node.attr.scale[0], node.attr.scale[1])
-  gl.uniform2f(program.rotation, node.attr.rotation[0], node.attr.rotation[1])
-  gl.uniform1f(program.opacity, node.attr.opacity)
-}
-
-function drawBuffer(buffer, type) {
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
-  gl.vertexAttribPointer(program.vertexPosition, buffer.itemSize, gl.FLOAT, false, 0, 0)
-  gl.drawArrays(type, 0, buffer.numItems)
-}
-
-function swapProgram(name) {
-  gl.useProgram(program = programs[name])
-  program.vertexPosition = gl.getAttribLocation(program, "attr")
-  gl.enableVertexAttribArray(program.vertexPosition)
-}
-
-function drawFill(node) {
-  swapProgram(isId(node.attr.fill) ? node.attr.fill : '_identity')
-  applyTransforms(node)
-  setDrawColor(d3.rgb(node.attr.fill))
-  drawBuffer(node.buffer, gl.TRIANGLE_FAN)
-}
-
-function drawStroke(node) {
-  gl.lineWidth(node.attr['stroke-width'])
-  swapProgram(isId(node.attr.stroke) ? node.attr.stroke : '_identity')
-  applyTransforms(node)
-  setDrawColor(d3.rgb(node.attr.stroke))
-  if (node.path)
-    for (var i = 0; i < node.path.length; i++)
-      drawBuffer(node.path[i], gl.LINE_LOOP)
-}
-
-function setDrawColor (c) {
-  gl.uniform4f(program.rgb,
-               c.r / 256,
-               c.g / 256,
-               c.b / 256,
-               1.0)
-}
-
-function buildBuffer(points) {
-  var buffer = gl.createBuffer()
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(points), gl.STATIC_DRAW)
-  buffer.itemSize = 3
-  buffer.numItems = points.length / buffer.itemSize
-  return buffer
-}
-
 function createTarget( width, height ) {
   var target = {}
   target.framebuffer = gl.createFramebuffer()
