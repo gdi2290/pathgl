05d6c3547a2f7b0f82313820b986f0e6de525e0f
adnan wahab <mail@adnanwahab.com>
2014-01-01 23:38:46 -0600

    add audio vis

---
+++ examples/waveform.js
@@ -0,0 +1,16 @@
+examples.waveform = function (selection) {
+  var s = d3.select(selection)
+          .attr(size)
+          .call(pathgl)
+  var scale = Math.PI * 2 / 30
+
+  s.selectAll('line').data(d3.range(30))
+  .enter()
+  .append('line')
+  .attr({
+    x1: 300
+  , y1: 300
+  , x2: function (d) { return Math.cos(d * scale) * 1000 }
+  , y2: function (d) { return Math.sin(d * scale) * 1000 }
+  })
+}
