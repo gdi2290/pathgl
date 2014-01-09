da8433a0d6e633170f2af6af500424bc900baadb
Adnan Wahab <mail@adnanwahab.com>
2014-01-08 16:12:00 -0800

    only add dimensions if no canvas was provided

--- src/start.js
+++ src/start.js
@@ -5,10 +5,8 @@ pathgl.stop = d3.functor()
 function pathgl(canvas) {
   var gl, program, programs
 
-  if (canvas == null) canvas = document.body.appendChild(document.createElement('canvas'))
-
-  canvas.width = canvas.width || 960
-  canvas.height = canvas.height || 500
+  if (canvas == null)
+    canvas = document.body.appendChild(extend(document.createElement('canvas'), { height: 500, width: 960 }))
 
   canvas = 'string' == typeof canvas ? document.querySelector(canvas) :
     canvas instanceof d3.selection ? canvas.node() :
