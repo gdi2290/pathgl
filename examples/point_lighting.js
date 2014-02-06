217ab29a4f7218e676b636ef1dabed4d214870e2
Adnan Wahab <mail@adnanwahab.com>
2014-02-05 22:09:26 -0800

    coool pattern

--- examples/point_lighting.js
+++ examples/point_lighting.js
@@ -1,6 +1,6 @@
 examples.swarm = function (selector) {
   var inlining = selector == 'canvas'
-  var data = d3.range(selector == 'canvas' ? 2e5 : 2000).map(function() {
+  var data = d3.range(selector == 'canvas' ? 1e4 : 2000).map(function() {
                return { xloc: 0, yloc: 0, xvel: 0, yvel: 0 }
              })
 
@@ -24,12 +24,15 @@ examples.swarm = function (selector) {
            .enter().append("circle")
            .attr('fill', random_hue)
            .attr("cx", 10)
-           .attr("cy", 10)
+           .attr("cy", function (d, i) { return data.negth  -  i})
            .attr('cz', function (d, i) { return i })
-           .attr("r", 2)
-               .shader({ cx: 'mod(pos.w + clock, 1000.) * 2.;'
-                   , cy: 'floor(pos.w / 100.);'
-                   })
+           .attr("r", function (d, i) { return i % 100 })
+            .shader({
+              cx: 'resolution.x / 2. + cos(pos.w + clock * .0001) * pos.z * 3.;'
+            , cy: 'resolution.y / 2. + sin(pos.w + clock * .0001) * pos.z * 3.;'
+            , stroke: 'vec4(unpack_color(stroke), .6);'
+            , radius: '5.;'
+                    })
 }
 
 function random_hue () { return "hsl(" + Math.random() * 360 + ",100%, 50%)" }
